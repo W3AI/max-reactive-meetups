02b0a0f92a37d58264a00da51b188b4b5db35c62
stefian <v_ianta@yahoo.com>
2019-05-19 23:32:06 -0400

    added meetup section in App comp

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,41 @@
 <script>
-    import Header from './UI/Header.svelte';
-    import MeetupItem from './Meetups/MeetupItem.svelte';
+  import Header from "./UI/Header.svelte";
+  import MeetupItem from "./Meetups/MeetupItem.svelte";
 
-    const meetups = [
-        {
-            id: 'm1',
-            title: 'Coding Smart Apps',
-            subtitle: 'Learn smart web coding in 2 hours',
-            description: 'In this meetup, we will have some experts that teach you how to code for the smart web!',
-            imageUrl: 'https://venturebeat.com/wp-content/uploads/2015/11/coding.jpg',
-            address: '247 Nerd Road, 36500 New York',
-            contactEmail: 'innovate@w3ai.net'
-        },
-        {
-            id: 'm2',
-            title: 'Swim Togethr',
-            subtitle: 'Let\'s go for some swiming',
-            description: 'We will simply swim some rounds!',
-            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Natacio.jpg/220px-Natacio.jpg',
-            address: '247 Nerd Road, 36500 New York',
-            contactEmail: 'swim@w3ai.net'
-        },
-
-    ];
+  const meetups = [
+    {
+      id: "m1",
+      title: "Coding Smart Apps",
+      subtitle: "Learn smart web coding in 2 hours",
+      description:
+        "In this meetup, we will have some experts that teach you how to code for the smart web!",
+      imageUrl: "https://venturebeat.com/wp-content/uploads/2015/11/coding.jpg",
+      address: "247 Nerd Road, 36500 New York",
+      contactEmail: "innovate@w3ai.net"
+    },
+    {
+      id: "m2",
+      title: "Swim Togethr",
+      subtitle: "Let's go for some swiming",
+      description: "We will simply swim some rounds!",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Natacio.jpg/220px-Natacio.jpg",
+      address: "247 Nerd Road, 36500 New York",
+      contactEmail: "swim@w3ai.net"
+    }
+  ];
 </script>
+
+<style>
+  #meetups {
+    margin-top: 5rem;
+  }
+</style>
 
 <Header />
 
-{#each meetups as meetup}
+<section id="meetups">
+  {#each meetups as meetup}
     <MeetupItem />
-{/each}
+  {/each}
+</section>
