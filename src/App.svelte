ed7b0e93b8ff718fd8d7f354e7d53e2357fd8076
stefian <v_ianta@yahoo.com>
2019-05-20 08:03:48 -0400

    linked meetups array to MeetupItem component

--- src/App.svelte
+++ src/App.svelte
@@ -8,14 +8,14 @@
       title: "Coding Smart Apps",
       subtitle: "Learn smart web coding in 2 hours",
       description:
-        "In this meetup, we will have some experts that teach you how to code for the smart web!",
+        "In this meetup, experts will teach you how to code for the smart web!",
       imageUrl: "https://venturebeat.com/wp-content/uploads/2015/11/coding.jpg",
       address: "247 Nerd Road, 36500 New York",
       contactEmail: "innovate@w3ai.net"
     },
     {
       id: "m2",
-      title: "Swim Togethr",
+      title: "Swim Together",
       subtitle: "Let's go for some swiming",
       description: "We will simply swim some rounds!",
       imageUrl:
@@ -36,6 +36,13 @@
 
 <section id="meetups">
   {#each meetups as meetup}
-    <MeetupItem />
+    <MeetupItem 
+        title={meetup.title} 
+        subtitle={meetup.subtitle}
+        description={meetup.description}
+        imageUrl={meetup.imageUrl}
+        email={meetup.contactEmail}
+        address={meetup.address}
+    />
   {/each}
 </section>
