4015635ea228ac387ca6fa0aee3b307d1adb30be
stefian <v_ianta@yahoo.com>
2019-05-21 09:28:36 -0400

    added Modal to the EditMeetup comp

--- src/Meetups/EditMeetup.svelte
+++ src/Meetups/EditMeetup.svelte
@@ -3,6 +3,7 @@
 
   import TextInput from "../UI/TextInput.svelte";
   import Button from "../UI/Button.svelte";
+  import Modal from "../UI/Modal.svelte";
 
   let title = "";
   let subtitle = "";
@@ -33,38 +34,40 @@
   }
 </style>
 
-<form on:submit|preventDefault={submitForm}>
-  <TextInput
-    id="title"
-    label="Title"
-    value={title}
-    on:input={event => (title = event.target.value)} />
-  <TextInput
-    id="subtitle"
-    label="Subitle"
-    value={subtitle}
-    on:input={event => (subtitle = event.target.value)} />
-  <TextInput
-    id="address"
-    label="Address"
-    value={address}
-    on:input={event => (address = event.target.value)} />
-  <TextInput
-    id="imageUrl"
-    label="Image URL"
-    value={imageUrl}
-    on:input={event => (imageUrl = event.target.value)} />
-  <TextInput
-    id="email"
-    label="E-Mail"
-    type="email"
-    value={email}
-    on:input={event => (email = event.target.value)} />
-  <TextInput
-    id="description"
-    label="Description"
-    controlType="textarea"
-    value={description}
-    on:input={event => (description = event.target.value)} />
-  <Button type="submit">Save</Button>
-</form>
+<Modal title="Edit Meetup Data">
+  <form on:submit|preventDefault={submitForm}>
+    <TextInput
+      id="title"
+      label="Title"
+      value={title}
+      on:input={event => (title = event.target.value)} />
+    <TextInput
+      id="subtitle"
+      label="Subitle"
+      value={subtitle}
+      on:input={event => (subtitle = event.target.value)} />
+    <TextInput
+      id="address"
+      label="Address"
+      value={address}
+      on:input={event => (address = event.target.value)} />
+    <TextInput
+      id="imageUrl"
+      label="Image URL"
+      value={imageUrl}
+      on:input={event => (imageUrl = event.target.value)} />
+    <TextInput
+      id="email"
+      label="E-Mail"
+      type="email"
+      value={email}
+      on:input={event => (email = event.target.value)} />
+    <TextInput
+      id="description"
+      label="Description"
+      controlType="textarea"
+      value={description}
+      on:input={event => (description = event.target.value)} />
+    <Button type="submit">Save</Button>
+  </form>
+</Modal>
