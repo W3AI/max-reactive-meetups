fe376e758b057977576683e869fba98dbbfdb966
stefian <v_ianta@yahoo.com>
2019-05-23 19:07:17 -0400

    added active class to All/Favorites buttons

--- src/Meetups/MeetupFilter.svelte
+++ src/Meetups/MeetupFilter.svelte
@@ -2,6 +2,8 @@
   import { createEventDispatcher } from "svelte";
 
   const dispatch = createEventDispatcher();
+
+  let selectedButton = 0;
 </script>
 
 <style>
@@ -42,14 +44,18 @@
 <div>
   <button
     type="button"
+    class:active={selectedButton === 0}
     on:click={() => {
+        selectedButton = 0;
       dispatch('select', 0);
     }}>
     All
   </button>
   <button
     type="button"
+    class:active={selectedButton === 1}
     on:click={() => {
+        selectedButton = 1;
       dispatch('select', 1);
     }}>
     Favorites
