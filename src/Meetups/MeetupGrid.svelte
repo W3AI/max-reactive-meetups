65285b45c5fbf6443496b52d08589bd1d578e00a
stefian <v_ianta@yahoo.com>
2019-05-24 10:32:17 -0400

    added transition:scale and animate:flip for items in grid

--- src/Meetups/MeetupGrid.svelte
+++ src/Meetups/MeetupGrid.svelte
@@ -1,55 +1,57 @@
 <script>
-    import { createEventDispatcher } from 'svelte';
-    import MeetupItem from './MeetupItem.svelte';
-    import MeetupFilter from "./MeetupFilter.svelte";
-    import Button from '../UI/Button.svelte';
+  import { createEventDispatcher } from "svelte";
+  import { scale } from "svelte/transition";
+  import { flip } from "svelte/animate";
+  import MeetupItem from "./MeetupItem.svelte";
+  import MeetupFilter from "./MeetupFilter.svelte";
+  import Button from "../UI/Button.svelte";
 
-    export let meetups;
+  export let meetups;
 
-    const dispatch = createEventDispatcher();
+  const dispatch = createEventDispatcher();
 
-    let favsOnly = false;
+  let favsOnly = false;
 
-    $: filteredMeetups = favsOnly ?  meetups.filter(m => m.isFavorite) : meetups;
+  $: filteredMeetups = favsOnly ? meetups.filter(m => m.isFavorite) : meetups;
 
-    function setFilter(event) {
-        favsOnly = event.detail === 1;
-    }
+  function setFilter(event) {
+    favsOnly = event.detail === 1;
+  }
 </script>
 
 <style>
   #meetups {
-  width: 100%;
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-gap: 1rem;
-}
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 1rem;
+  }
 
-#meetup-controls {
+  #meetup-controls {
     margin: 1rem;
     display: flex;
     justify-content: space-between;
-}
-
-@media (min-width: 768px) {
-  #meetups {
-    grid-template-columns: repeat(2, 1fr);
   }
-}
 
+  @media (min-width: 768px) {
+    #meetups {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
 </style>
 
 <section id="meetup-controls">
-    <MeetupFilter on:select="{setFilter}" />
+  <MeetupFilter on:select={setFilter} />
 
-    <Button on:click={() => dispatch('add') }>Add Service</Button>
+  <Button on:click={() => dispatch('add')}>Add Service</Button>
 </section>
 
 <section id="meetups">
-  {#each filteredMeetups as meetup}
-    <MeetupItem 
+  {#each filteredMeetups as meetup (meetup.id)}
+    <div transition:scale animate:flip={{duration: 300}} >
+      <MeetupItem
         id={meetup.id}
-        title={meetup.title} 
+        title={meetup.title}
         subtitle={meetup.subtitle}
         description={meetup.description}
         imageUrl={meetup.imageUrl}
@@ -58,5 +60,6 @@
         isFav={meetup.isFavorite}
         on:showdetails
         on:edit />
+    </div>
   {/each}
 </section>
